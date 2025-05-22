81ef2cff782b076c76010fa2f37cbb235e428405
Paulo Barros <paulo.barros@lambda3.com.br>
2025-05-22 11:08:05 -0300

    chore: configurar 300kbs

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,8 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
-    //bodyParser: false,
-  });
-  app.useBodyParser('json', { limit: '600kb' });
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useBodyParser('json', { limit: '300kb' });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
